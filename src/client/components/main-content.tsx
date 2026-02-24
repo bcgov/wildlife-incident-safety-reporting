94ef01b7dbcb3d7083295695f307757c503c6dcc
Jayman McAllister <jayman.mcallister@gov.bc.ca>
2026-02-23 16:21:01 -0800

    Enhance map controls with positioning and features

--- src/client/components/main-content.tsx
+++ src/client/components/main-content.tsx
@@ -3,7 +3,7 @@ import { MapControls, Map as MapView } from '@/components/ui/map'
 export function MainContent() {
   return (
     <MapView className="flex-1" center={[-124.5, 54.5]} zoom={5}>
-      <MapControls showZoom showCompass />
+      <MapControls position="top-left" showZoom showCompass showLocate showFullscreen />
     </MapView>
   )
 }
